3204a1025ca8c10c9c6b3bdb62dd7b02450dd7a1
cleilson pereira <marcialwushu@hotmail.com>
2021-05-29 20:38:24 -0300

    :fire: Add gRPC Client

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "DevComputaria" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+            Console.ReadLine();
+        }
+    }
+}
